30d2703c797761492ce03ae97a86f525a0576eed
Pelikano <ltvanderkrogt@gmail.com>
2024-03-05 20:47:02 +0100

    Update d3_sensor.cpp

--- components/pm-d3/d3_sensor.cpp
+++ components/pm-d3/d3_sensor.cpp
@@ -1,7 +1,7 @@
-// esphome/external_components/d3_sensor/d3_sensor.cpp
+// config/d3_sensor/d3_sensor.cpp
 
-#include "d3_sensor.h"
 #include "esphome/core/log.h"
+#include "d3_sensor.h"
 
 namespace esphome {
 namespace d3_sensor {
@@ -19,7 +19,7 @@ void D3Sensor::process_data() {
             uint16_t calculated_checksum = this->calculate_checksum(data);
 
             if (received_checksum == 0xFFFF || received_checksum == calculated_checksum) {
-                this->print_values(data);
+                this->publish_state(data);
             }
         }
     }
@@ -33,13 +33,7 @@ uint16_t D3Sensor::calculate_checksum(const std::array<uint8_t, 32> &data) {
     return checksum;
 }
 
-void D3Sensor::print_values(const std::array<uint8_t, 32> &data) {
-    int pm1_standard = (data[4] << 8) | data[5];
-    int pm25_standard = (data[6] << 8) | data[7];
-    int pm10_standard = (data[8] << 8) | data[9];
-
-    ESP_LOGD(TAG, "PM1.0: %d ug/m3, PM2.5: %d ug/m3, PM10: %d ug/m3", pm1_standard, pm25_standard, pm10_standard);
-}
+void D3Sensor::dump_config() { LOG_SENSOR("", "D3 Sensor", this); }
 
 }  // namespace d3_sensor
 }  // namespace esphome
