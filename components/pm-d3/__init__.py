6dfa5a439de49f7e10436aa1dacd42e1905759ee
Pelikano <ltvanderkrogt@gmail.com>
2024-03-31 05:14:28 +0200

    Delete components/pm-d3 directory

--- components/pm-d3/__init__.py
+++
@@ -1,9 +0,0 @@
-# config/d3_sensor/__init__.py
-
-DEPENDENCIES = ["uart"]
-
-def to_code(config):
-    var = cg.new_Pvariable(config[CONF_ID])
-    await cg.register_component(var, config)
-    await uart.register_uart_device(var, config)
-    cg.add(var.set_data(config[CONF_DATA]))
